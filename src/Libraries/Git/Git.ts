cd419c8c076b464cd740c5f7b28b2de707c0b623
GeorgeSaeed <george.saeed@softec.ai>
2024-06-22 18:23:05 +0300

    getBranchNames, getCurrentBranch added to class Git

--- src/Libraries/Git/Git.ts
+++ src/Libraries/Git/Git.ts
@@ -3,9 +3,9 @@ import { Shell } from "../../Shell";
 
 export class Git
 {
-    static async GetRemoteOriginAsync(): Promise<string>
+    static async getRemoteOriginAsync(): Promise<string>
     {
-        const result = await Shell.RunForStdoutAsync("git remote -v");
+        const result = await Shell.RunForStdout("git remote -v");
         const matches = Regex.of(/\s/g).splitByMatches(result);
         if(matches.length < 2)
         {
@@ -15,4 +15,26 @@ export class Git
 
         return matches[1].value;
     } 
+
+    static getBranchNames()
+    {
+        const result = Shell.RunForStdout("git branch");
+        const names = Regex.of(/\n/g).splitByMatches(result).map(b=>b.value).filter(name => !!name).map(name => name.replace("*", "").trim());
+
+        return names;
+    }
+
+    static getCurrentBranch(): string
+    {
+        const result = Shell.RunForStdout("git branch");
+        const names = Regex.of(/\n/g).splitByMatches(result).map(b=>b.value).filter(name => !!name && name.trim().startsWith("*"))
+                                                                            .map(name => name.trim().substring(1));
+
+        if(names.length !== 1) 
+        {
+            Shell.terminate("Couldn't read the default branch name");
+        }
+
+        return names[0];
+    }
 }
